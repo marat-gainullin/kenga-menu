38969a724787daadb78b900f78e56240c9656abf
Marat Gainullin <m.gainullin@PTCGPS.local>
2023-03-07 10:19:23 +0100

    Improvements

--- src/menu.js
+++ src/menu.js
@@ -71,13 +71,13 @@ class Menu extends Container {
         });
 
         function showAt(left, top) {
-            if (self.element.parentElement)
-                throw 'Menu is already shown';
             self.element.classList.remove('p-menu-horizontal-rel');
             self.element.classList.remove('p-menu-vertical-rel');
             self.element.style.top = `${top}px`;
             self.element.style.left = `${left}px`;
-            document.body.appendChild(self.element);
+            if (!self.element.parentElement) {
+              document.body.appendChild(self.element);
+            }
             if (left + self.element.offsetWidth > window.innerWidth) {
                 self.element.style.left = `${left - self.element.offsetWidth}px`;
             }
