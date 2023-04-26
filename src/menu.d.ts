c1e77de733f4f96a5ed8ea17e6ecc1a14047cf61
marat-gainullin <marat.gainullin.iv@gmail.com>
2023-04-26 22:12:47 +0200

    Improvements

--- src/menu.d.ts
+++ src/menu.d.ts
@@ -1,8 +1,8 @@
 import Container from 'kenga/container'
 
 export default class Menu extends Container {
-  showRelativeTo(anElement: HTMLElement, horizontal: boolean, ltr?: boolean, ttb?: boolean): void
-  popupRelativeTo(anElement: HTMLElement, horizontal: boolean, ltr?: boolean, ttb?: boolean): void
+  showRelativeTo(anElement: HTMLElement, horizontal: boolean, leftToRight?: boolean, topToBottom?: boolean): void
+  popupRelativeTo(anElement: HTMLElement, horizontal: boolean, leftToRight?: boolean, topToBottom?: boolean): void
   showAt(left: number, top: number): void
   popupAt(left: number, top: number): void
   close(): void
--- src/menu.js
+++ src/menu.js
@@ -19,39 +19,69 @@ class Menu extends Container {
                 `;
         this.element.appendChild(gapsStyle);
 
-        function showRelativeTo(anElement, horizontal = true, ltr = true, ttb = true) {
+        function showRelativeTo(anElement, horizontal = false, leftToRight = true, topToBottom = true) {
             if (!self.element.parentElement) {
                 const targetTop = Ui.absoluteTop(anElement);
                 const targetLeft = Ui.absoluteLeft(anElement);
-                if (horizontal) {
-                    self.element.classList.remove('p-menu-horizontal-rel');
-                    self.element.classList.add('p-menu-vertical-rel');
-                    const top = targetTop;
-                    const left = targetLeft + anElement.offsetWidth;
-                    self.element.style.top = `${top}px`;
-                    self.element.style.left = `${left}px`;
-                    document.body.appendChild(self.element);
-                    if (!ltr || left + self.element.offsetWidth > window.innerWidth) {
-                        self.element.style.left = `${targetLeft - self.element.offsetWidth}px`;
-                    }
-                    if (!ttb || top + self.element.offsetHeight > window.innerHeight) {
-                        self.element.style.top = `${targetTop + anElement.offsetHeight - self.element.offsetHeight}px`;
-                    }
-                } else {
-                    self.element.classList.remove('p-menu-vertical-rel');
-                    self.element.classList.add('p-menu-horizontal-rel');
-                    const top = targetTop + anElement.offsetHeight;
-                    const left = targetLeft;
-                    self.element.style.top = `${top}px`;
-                    self.element.style.left = `${left}px`;
-                    document.body.appendChild(self.element);
-                    if (!ltr || left + self.element.offsetWidth > window.innerWidth) {
-                        self.element.style.left = `${targetLeft + anElement.offsetWidth - self.element.offsetWidth}px`;
-                    }
-                    if (!ttb || top + self.element.offsetHeight > window.innerHeight) {
-                        self.element.style.top = `${targetTop - self.element.offsetHeight}px`;
-                    }
+
+                function tryToPlace(ltr, ttb) {
+                  if (horizontal) {
+                      self.element.classList.remove('p-menu-horizontal-rel');
+                      self.element.classList.add('p-menu-vertical-rel');
+                      if (ttb) {
+                          self.element.style.top = `${targetTop}px`;
+                          self.element.style.bottom = ''
+                      } else {
+                          self.element.style.top = '';
+                          self.element.style.bottom = `${document.body.clientHeight - targetTop - anElement.offsetHeight}px`;
+                      }
+                      if (ltr) {
+                          self.element.style.left = `${targetLeft + anElement.offsetWidth}px`;
+                          self.element.style.right = '';
+                      } else {
+                          self.element.style.left = '';
+                          self.element.style.right = `${document.body.clientWidth - targetLeft}px`;
+                      }
+                  } else {
+                      self.element.classList.remove('p-menu-vertical-rel');
+                      self.element.classList.add('p-menu-horizontal-rel');
+                      if (ttb) {
+                          self.element.style.top = `${targetTop + anElement.offsetHeight}px`;
+                          self.element.style.bottom = ''
+                      } else {
+                          self.element.style.top = '';
+                          self.element.style.bottom = `${document.body.clientHeight - targetTop}px`;
+                      }
+                      if (ltr) {
+                          self.element.style.left = `${targetLeft}px`;
+                          self.element.style.right = '';
+                      } else {
+                          self.element.style.left = '';
+                          self.element.style.right = `${document.body.clientWidth - targetLeft - anElement.offsetWidth}px`;
+                      }
+                  }
                 }
+                tryToPlace(leftToRight, topToBottom)
+                document.body.appendChild(self.element);
+                if (topToBottom) {
+                  if (self.element.offsetTop + self.element.offsetHeight > document.body.clientHeight) {
+                    topToBottom = !topToBottom
+                  }
+                } else {
+                  if (self.element.offsetTop < 0) {
+                    topToBottom = !topToBottom
+                  }
+                }
+                if (leftToRight) {
+                  if (self.element.offsetLeft + self.element.offsetWidth > document.body.clientWidth) {
+                    leftToRight = !leftToRight
+                  }
+                } else {
+                  if (self.element.offsetLeft < 0) {
+                    leftToRight = !leftToRight
+                  }
+                }
+                tryToPlace(leftToRight, topToBottom)
             }
         }
         Object.defineProperty(this, 'showRelativeTo', {
