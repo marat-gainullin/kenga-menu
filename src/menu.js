15b5e42eeeb540828a2a1ced96dcdd361493038f
marat-gainullin <marat.gainullin.iv@gmail.com>
2024-10-27 18:54:49 +0100

    Improvements

--- src/menu.js
+++ src/menu.js
@@ -1,88 +1,15 @@
-import Ui from 'kenga/utils';
 import Container from 'kenga/container';
+import Popup from 'kenga/popup';
 
 class Menu extends Container {
     constructor() {
         super();
-        const self = this;
 
         this.element.classList.add('p-menu');
-        this.element.id = `p-${Ui.next()}`;
-
-        const gapsStyle = document.createElement('style');
-        gapsStyle.innerHTML =
-                `
-                div#${self.element.id} > .p-widget {display: block;}
-                div#${self.element.id} > .p-tabs {display: flex;}
-                div#${self.element.id} > .p-holy-grail-column {display: flex;}
-                div#${self.element.id} > .p-box-vertical {display: flex;}
-                `;
-        this.element.appendChild(gapsStyle);
+        const popup = new Popup(this.element)
 
         function showRelativeTo(anElement, horizontal = false, leftToRight = true, topToBottom = true) {
-            if (!self.element.parentElement) {
-                const targetTop = Ui.absoluteTop(anElement);
-                const targetLeft = Ui.absoluteLeft(anElement);
-
-                function tryToPlace(ltr, ttb) {
-                  if (horizontal) {
-                      self.element.classList.remove('p-menu-horizontal-rel');
-                      self.element.classList.add('p-menu-vertical-rel');
-                      if (ttb) {
-                          self.element.style.top = `${targetTop}px`;
-                          self.element.style.bottom = ''
-                      } else {
-                          self.element.style.top = '';
-                          self.element.style.bottom = `${document.body.clientHeight - targetTop - anElement.offsetHeight}px`;
-                      }
-                      if (ltr) {
-                          self.element.style.left = `${targetLeft + anElement.offsetWidth}px`;
-                          self.element.style.right = '';
-                      } else {
-                          self.element.style.left = '';
-                          self.element.style.right = `${document.body.clientWidth - targetLeft}px`;
-                      }
-                  } else {
-                      self.element.classList.remove('p-menu-vertical-rel');
-                      self.element.classList.add('p-menu-horizontal-rel');
-                      if (ttb) {
-                          self.element.style.top = `${targetTop + anElement.offsetHeight}px`;
-                          self.element.style.bottom = ''
-                      } else {
-                          self.element.style.top = '';
-                          self.element.style.bottom = `${document.body.clientHeight - targetTop}px`;
-                      }
-                      if (ltr) {
-                          self.element.style.left = `${targetLeft}px`;
-                          self.element.style.right = '';
-                      } else {
-                          self.element.style.left = '';
-                          self.element.style.right = `${document.body.clientWidth - targetLeft - anElement.offsetWidth}px`;
-                      }
-                  }
-                }
-                tryToPlace(leftToRight, topToBottom)
-                document.body.appendChild(self.element);
-                if (topToBottom) {
-                  if (self.element.offsetTop + self.element.offsetHeight > document.body.clientHeight) {
-                    topToBottom = !topToBottom
-                  }
-                } else {
-                  if (self.element.offsetTop < 0) {
-                    topToBottom = !topToBottom
-                  }
-                }
-                if (leftToRight) {
-                  if (self.element.offsetLeft + self.element.offsetWidth > document.body.clientWidth) {
-                    leftToRight = !leftToRight
-                  }
-                } else {
-                  if (self.element.offsetLeft < 0) {
-                    leftToRight = !leftToRight
-                  }
-                }
-                tryToPlace(leftToRight, topToBottom)
-            }
+            popup.showRelativeTo(anElement, horizontal, leftToRight, topToBottom)
         }
         Object.defineProperty(this, 'showRelativeTo', {
             get: function () {
@@ -90,9 +17,8 @@ class Menu extends Container {
             }
         });
 
-        function popupRelativeTo(anElement, horizontal = true, ltr = true, ttb = true) {
-            Ui.startMenuSession(self);
-            showRelativeTo(anElement, horizontal, ltr, ttb);
+        function popupRelativeTo(anElement, horizontal = true, leftToRight = true, topToBottom = true) {
+            popup.popupRelativeTo(anElement, horizontal, leftToRight, topToBottom)
         }
         Object.defineProperty(this, 'popupRelativeTo', {
             get: function () {
@@ -101,19 +27,7 @@ class Menu extends Container {
         });
 
         function showAt(left, top) {
-            self.element.classList.remove('p-menu-horizontal-rel');
-            self.element.classList.remove('p-menu-vertical-rel');
-            self.element.style.top = `${top}px`;
-            self.element.style.left = `${left}px`;
-            if (!self.element.parentElement) {
-              document.body.appendChild(self.element);
-            }
-            if (left + self.element.offsetWidth > window.innerWidth) {
-                self.element.style.left = `${left - self.element.offsetWidth}px`;
-            }
-            if (top + self.element.offsetHeight > window.innerHeight) {
-                self.element.style.top = `${top - self.element.offsetHeight}px`;
-            }
+            popup.showAt(left, top)
         }
         Object.defineProperty(this, 'showAt', {
             get: function () {
@@ -122,8 +36,7 @@ class Menu extends Container {
         });
 
         function popupAt(left, top) {
-            Ui.startMenuSession(self);
-            showAt(left, top);
+            popup.popupAt(left, top)
         }
         Object.defineProperty(this, 'popupAt', {
             get: function () {
@@ -133,18 +46,12 @@ class Menu extends Container {
 
         Object.defineProperty(this, 'shown', {
             get: function () {
-                return !!self.element.parentElement;
+                return popup.shown;
             }
         });
 
         function close() {
-            if (self.element.parentElement) {
-                self.element.parentElement.removeChild(self.element);
-                self.forEach(item => {
-                    if (item.subMenu)
-                        item.subMenu.close();
-                });
-            }
+            popup.close()
         }
         Object.defineProperty(this, 'close', {
             get: function () {
